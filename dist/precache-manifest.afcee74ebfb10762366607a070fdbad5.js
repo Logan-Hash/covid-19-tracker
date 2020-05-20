6117d85aff2d69e22d50a77e4d8b4744fe97e202
Logan-Hash <pyaephyothant1998@gmail.com>
2020-05-20 17:58:57 +0630

    Done

---
+++ dist/precache-manifest.afcee74ebfb10762366607a070fdbad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ad1a405a06ee418a07729c1474504e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d421bdadaed32432b6f7",
+    "url": "/static/css/main.52d5a6e2.chunk.css"
+  },
+  {
+    "revision": "20864b1da73ad39454e4",
+    "url": "/static/js/2.674aa906.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.674aa906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d421bdadaed32432b6f7",
+    "url": "/static/js/main.b23084a5.chunk.js"
+  },
+  {
+    "revision": "0b8e1d287ca3183b295f",
+    "url": "/static/js/runtime-main.ea409370.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
